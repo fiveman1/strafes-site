6656c3e99c2bb92373b6f612c029adc8db4f51ca
fiveman1 <carterpenterman@gmail.com>
2026-01-04 23:58:07 -0600

    tweak stuff and fix math

--- client/src/components/TimeDisplay.tsx
+++ client/src/components/TimeDisplay.tsx
@@ -24,14 +24,15 @@ function TimeDisplay(props: ITimeDisplayProps) {
         theme.palette.mode === "dark" ? green["A400"] : darken(green["A400"], 0.3);
 
     if (diff && diff > 0) {
-        const wrTime = ms + diff;
+        const wrTime = ms - diff;
         let ratio = diff / wrTime;
+        const maxRatio = 0.05;
         // Desaturate the color, if it's 10% worse than WR then use full saturation,
         // otherwise we will scale it linearly on a normalized scale between 0% to 10%.
-        if (ratio > 0.1) {
-            ratio = 0.1;
+        if (ratio > maxRatio) {
+            ratio = maxRatio;
         }
-        ratio = normalize(ratio, 0, 0.1, 0.55, 1);
+        ratio = normalize(ratio, 0, maxRatio, 0.55, 1);
 
         const hsl = convertToHSL(diffColor);
         hsl.s *= ratio;
