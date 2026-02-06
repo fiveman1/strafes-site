37527c5c73974a2b92a54656519ee4f87621d50a
fiveman1 <carterpenterman@gmail.com>
2026-02-06 00:18:27 -0600

    handle response errors

--- server/src/strafes_api/api.ts
+++ server/src/strafes_api/api.ts
@@ -3,6 +3,7 @@ import { Api } from "./client.js";
 import memoize from "memoize";
 import { Game, RankSortBy, Style, TimeSortBy } from "shared";
 
+const IS_DEBUG = process.env.DEBUG === "true";
 const STRAFES_KEY = process.env.STRAFES_KEY;
 if (!STRAFES_KEY) {
     console.error("Missing StrafesNET API key");
@@ -21,23 +22,43 @@ const STRAFES_CLIENT = new Api({
 });
 
 export async function getPlacements(timeIds: string[]) {
-    const res = await STRAFES_CLIENT.time.placementList({ids: timeIds.join(",")});
+    let res;
+    try {
+        res = await STRAFES_CLIENT.time.placementList({ids: timeIds.join(",")});
+    }
+    catch (err) {
+        if (IS_DEBUG) {
+            console.error(err);
+        }
+        return undefined;
+    }
+
     if (!res.ok) return undefined;
+
     return res.data.data;
 }
 
 export const getTimes = memoize(getTimesCore, {cacheKey: JSON.stringify, maxAge: 5 * 60 * 1000});
 async function getTimesCore(userId: string | number | undefined, mapId: string | number | undefined, pageSize: number, pageNum: number, game: Game | undefined, style: Style | undefined, course: number, sort?: TimeSortBy) {
-    const res = await STRAFES_CLIENT.time.timeList({
-        user_id: userId !== undefined ? +userId : undefined,
-        map_id: mapId !== undefined ? +mapId : undefined,
-        page_size: pageSize,
-        page_number: pageNum,
-        game_id: game === Game.all ? undefined : game,
-        style_id: style === Style.all ? undefined : style,
-        mode_id: course >= 0 ? course : undefined,
-        sort_by: sort
-    });
+    let res;
+    try {
+        res = await STRAFES_CLIENT.time.timeList({
+            user_id: userId !== undefined ? +userId : undefined,
+            map_id: mapId !== undefined ? +mapId : undefined,
+            page_size: pageSize,
+            page_number: pageNum,
+            game_id: game === Game.all ? undefined : game,
+            style_id: style === Style.all ? undefined : style,
+            mode_id: course >= 0 ? course : undefined,
+            sort_by: sort
+        });
+    }
+    catch (err) {
+        if (IS_DEBUG) {
+            console.error(err);
+        }
+        return undefined;
+    }
     
     if (!res.ok) return undefined;
     
@@ -46,11 +67,20 @@ async function getTimesCore(userId: string | number | undefined, mapId: string |
 
 export const getUserRank = memoize(getUserRankCore, {cacheKey: JSON.stringify, maxAge: 5 * 60 * 1000});
 async function getUserRankCore(userId: string | number, game: Game, style: Style) {
-    const res = await STRAFES_CLIENT.user.rankList(+userId, {
-        game_id: game,
-        style_id: style,
-        mode_id: 0
-    });
+    let res;
+    try {
+        res = await STRAFES_CLIENT.user.rankList(+userId, {
+            game_id: game,
+            style_id: style,
+            mode_id: 0
+        });
+    }
+    catch (err) {
+        if (IS_DEBUG) {
+            console.error(err);
+        }
+        return undefined;
+    }
     
     if (!res.ok) return undefined;
     
@@ -59,14 +89,23 @@ async function getUserRankCore(userId: string | number, game: Game, style: Style
 
 export const getRanks = memoize(getRanksCore, {cacheKey: JSON.stringify, maxAge: 5 * 60 * 1000});
 async function getRanksCore(pageSize: number, pageNum: number, game: Game, style: Style, sort: RankSortBy) {
-    const res = await STRAFES_CLIENT.rank.rankList({
-        page_size: pageSize,
-        page_number: pageNum,
-        sort_by: sort,
-        game_id: game,
-        style_id: style,
-        mode_id: 0
-    });
+    let res;
+    try {
+        res = await STRAFES_CLIENT.rank.rankList({
+            page_size: pageSize,
+            page_number: pageNum,
+            sort_by: sort,
+            game_id: game,
+            style_id: style,
+            mode_id: 0
+        });
+    }
+    catch (err) {
+        if (IS_DEBUG) {
+            console.error(err);
+        }
+        return undefined;
+    }
     
     if (!res.ok) return undefined;
     
@@ -75,7 +114,16 @@ async function getRanksCore(pageSize: number, pageNum: number, game: Game, style
 
 export const getUserInfo = memoize(getUserInfoCore, {cacheKey: JSON.stringify, maxAge: 5 * 60 * 1000});
 async function getUserInfoCore(userId: string | number) {
-    const res = await STRAFES_CLIENT.user.userDetail(+userId);
+    let res;
+    try {
+        res = await STRAFES_CLIENT.user.userDetail(+userId);
+    }
+    catch (err) {
+        if (IS_DEBUG) {
+            console.error(err);
+        }
+        return undefined;
+    }
     
     if (!res.ok) return undefined;
     
