afb963d45416057d4493fd7037bc2961e3e91004
fiveman1 <carterpenterman@gmail.com>
2026-02-07 00:26:25 -0600

    cache user thumbnails

--- server/src/users.ts
+++ server/src/users.ts
@@ -2,7 +2,9 @@ import { RowDataPacket } from "mysql2/promise";
 import { AUTH_POOL, SettingsRow } from "./oauth.js";
 import { tryGetRequest } from "./requests.js";
 import { UserInfo } from "shared";
+import memCache from "memory-cache";
 
+const cache: memCache.CacheClass<string, string> = new memCache.Cache();
 
 export async function setUserInfoForList(users: UserInfo[]) {
     if (users.length < 1) {
@@ -32,9 +34,29 @@ async function setProfileInfoForList(users: UserInfo[]) {
     }
 }
 
+function getUserThumbKey(userId: string | number) {
+    return `userThumb,${userId}`;
+}
+
 export async function setUserThumbsForList(users: UserInfo[]) {
+    const notCachedIds: string[] = [];
+
+    for (const user of users) {
+        const cacheThumb = cache.get(getUserThumbKey(user.userId));
+        if (cacheThumb === null) {
+            notCachedIds.push(user.userId);
+        }
+        else {
+            user.userThumb = cacheThumb;
+        }
+    }
+
+    if (notCachedIds.length < 1) {
+        return;
+    }
+
     const thumbRes = await tryGetRequest("https://thumbnails.roproxy.com/v1/users/avatar-headshot", {
-        userIds: users.map((user) => user.userId),
+        userIds: notCachedIds,
         size: "75x75",
         format: "Webp",
         isCircular: false
@@ -47,9 +69,12 @@ export async function setUserThumbsForList(users: UserInfo[]) {
     const idToThumb = new Map<number, string>();
     for (const thumbInfo of thumbRes?.data.data) {
         idToThumb.set(thumbInfo.targetId, thumbInfo.imageUrl);
+        cache.put(getUserThumbKey(thumbInfo.targetId), thumbInfo.imageUrl, 4 * 60 * 60 * 1000); // 4 hours
     }
 
     for (const user of users) {
-        user.userThumb = idToThumb.get(+user.userId);
+        if (user.userThumb === undefined) {
+            user.userThumb = idToThumb.get(+user.userId);
+        }
     }
 }
