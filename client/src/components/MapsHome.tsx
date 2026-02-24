05c782e0fb83d1db4c800450686fe6aab5e4c3b6
fiveman1 <carterpenterman@gmail.com>
2026-02-23 20:24:21 -0600

    progress

--- client/src/components/MapsHome.tsx
+++ client/src/components/MapsHome.tsx
@@ -7,8 +7,8 @@ import DownloadIcon from '@mui/icons-material/Download';
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import IconButton from "@mui/material/IconButton";
 import { ReactElement, useCallback, useEffect, useMemo, useState } from "react";
-import { ContextParams, mapsToCsv } from "../common/common";
-import { useTheme } from "@mui/material/styles";
+import { ContextParams, getGameColor, mapsToCsv } from "../common/common";
+import { darken, useTheme } from "@mui/material/styles";
 import { useOutletContext } from "react-router";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -16,11 +16,18 @@ import SearchIcon from '@mui/icons-material/Search';
 import Grid from "@mui/material/Grid";
 import Pagination from "@mui/material/Pagination";
 import { parseAsInteger, useQueryState } from "nuqs";
-import { Map as StrafesMap } from "shared";
+import { formatGame, formatTier, Map as StrafesMap } from "shared";
 import { filterMapsBySearch } from "../common/sort";
 import MapThumb from "./displays/MapThumb";
-import Paper from "@mui/material/Paper";
-import Divider from "@mui/material/Divider";
+import { getMapTierColor, UNRELEASED_MAP_COLOR } from "../common/colors";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import PersonIcon from '@mui/icons-material/Person';
+import { grey } from "@mui/material/colors";
+
+const shortDateFormat = Intl.DateTimeFormat(undefined, {
+    year: "numeric",
+    month: "short"
+});
 
 interface MapCardProps {
     map: StrafesMap
@@ -29,56 +36,191 @@ interface MapCardProps {
 function MapCard(props: MapCardProps) {
     const { map } = props;
     const theme = useTheme();
+    
+    const isLightMode = theme.palette.mode === "light";
+    const small = useMediaQuery("(max-width: 480px)");
+    const med = useMediaQuery("(max-width: 800px)");
+    const large = useMediaQuery("(max-width: 1225px)");
+    
+    let cardSize = 275;
+    if (small) cardSize = 160;
+    else if (med) cardSize = 190;
+    else if (large) cardSize = 230;
 
-    const cardSize = 250;
+    const mapDate = new Date(map.date);
+    const isUnreleased = new Date() < mapDate;
+
+    const nameSpace = 48;
+    const nameHeight = nameSpace + "px";
+
+    const creatorSpace = 36;
+    const creatorHeight = creatorSpace + "px";
+
+    const gameColor = getGameColor(map.game, theme);
+    const tierColor = getMapTierColor(map.tier);
 
     return (
         <Grid key={map.id} >
-            
-            <Box width={cardSize} height={cardSize + 100} display="flex" flexDirection="column">
-                
-                <Box height={cardSize} position="relative">
-                    <MapThumb size={cardSize} map={map} useLargeThumb  sx={{ zIndex: -1, borderRadius: "6px 6px 0 0" }}/>
-                    <Paper square elevation={1}
-                    sx={{
-                        position: "absolute",
-                        bottom: "0px",
-                        height: "48px",
-                        width: "100%",
-                        overflow:"hidden", 
-                        textOverflow:"ellipsis",
-                        boxShadow: 0
-                    }}
+            <Box 
+                width={cardSize} 
+                height={cardSize} 
+                display="flex" 
+                flexDirection="column"
+            >
+                <Box 
+                    height={cardSize} 
+                    position="relative" 
+                    borderRadius="6px" 
+                    boxShadow={2}
+                    bgcolor={isLightMode ? grey[400] : grey[800]}
+                >
+                    <MapThumb 
+                        size={cardSize} 
+                        map={map} 
+                        useLargeThumb  
+                        sx={{ 
+                            zIndex: -1, 
+                            borderRadius: "6px 6px 8px 8px", 
+                            border: 0 
+                        }}
+                    />
+                    <Box 
+                        display="flex" 
+                        position="absolute" 
+                        top="8px" 
+                        right="8px"
                     >
-                        <Box display="inline-block" height="48px" width={cardSize}>
+                        <Typography 
+                            variant="body2" 
+                            fontWeight="bold" 
+                            sx={{
+                                padding: 0.4,
+                                lineHeight: 1.1,
+                                overflow: "hidden",
+                                backgroundColor: gameColor,
+                                textAlign: "center",
+                                color: "white",
+                                textShadow: "black 1px 1px 1px",
+                                borderRadius: "6px"
+                            }}
+                        >
+                            {formatGame(map.game)}
+                        </Typography>
+                        <Typography 
+                            variant="body2" 
+                            fontWeight="bold" 
+                            ml={0.5} 
+                            sx={{
+                                padding: 0.4,
+                                lineHeight: 1.0,
+                                overflow: "hidden",
+                                backgroundColor: darken(tierColor, 0.4),
+                                textAlign: "center",
+                                color: "white",
+                                textShadow: "black 1px 1px 1px",
+                                borderRadius: "6px",
+                                border: 1,
+                                borderColor: tierColor
+                            }}
+                        >
+                            {formatTier(map.tier)}
+                        </Typography>
+                    </Box>
+                    <Typography 
+                        position="absolute" 
+                        top="4px" 
+                        left="4px" 
+                        variant="body1" 
+                        fontWeight="bold" 
+                        sx={{
+                            padding: 0.7,
+                            lineHeight: 1.1,
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            backdropFilter: "blur(8px)",
+                            textAlign: "center",
+                            color: "white",
+                            textShadow: "black 3px 3px 3px",
+                            borderRadius: "8px",
+                            bgcolor: isUnreleased ? UNRELEASED_MAP_COLOR + "80" : undefined
+                        }}
+                    >
+                        {shortDateFormat.format(mapDate)}
+                    </Typography>
+                    <Box
+                        sx={{
+                            position: "absolute",
+                            bottom: creatorHeight,
+                            height: nameHeight,
+                            width: "100%",
+                            overflow:"hidden", 
+                            textOverflow:"ellipsis",
+                            bgcolor: "#80808050",
+                            backdropFilter: "blur(16px)", 
+                        }}
+                    >
+                        <Box 
+                            display="inline-flex" 
+                            height={nameHeight} 
+                            width={cardSize} 
+                            pl={1.25}
+                            pr={1.25}
+                            alignItems="center"
+                        >
                             <Typography 
-                                variant="h6" 
+                                variant="h5" 
+                                title={map.name}
                                 fontWeight="bold"
-                                height="48px"
-                                width={cardSize}
+                                color="white"
+                                overflow="hidden"
+                                textOverflow="ellipsis"
                                 whiteSpace="nowrap"
                                 sx={{
-                                    padding: 1,
-                                    overflow:"hidden", 
-                                    textOverflow:"ellipsis", 
-                                    textAlign: "center"
+                                    textShadow: "black 1px 1px 1px"
                                 }}
                             >
                                 {map.name}
                             </Typography>
                         </Box>
-                    </Paper>
-                    
+                    </Box>
+                    <Box
+                        sx={{
+                            position: "absolute",
+                            bottom: "0px",
+                            height: creatorHeight,
+                            width: "100%",
+                            borderRadius: "0 0 6px 6px", 
+                            boxShadow: 0,
+                            bgcolor: darken(tierColor, 0.25)
+                        }}
+                    >
+                        <Box 
+                            display="inline-flex" 
+                            height={creatorHeight} 
+                            width={cardSize} 
+                            justifyContent="flex-end" 
+                            alignItems="center" 
+                            p={1}
+                        >
+                            <PersonIcon 
+                                fontSize="inherit" 
+                                htmlColor="white"
+                            />
+                            <Typography 
+                                variant="subtitle2" 
+                                color="white"
+                                title={map.creator}
+                                ml={0.5}
+                                overflow="hidden"
+                                textOverflow="ellipsis"
+                                whiteSpace="nowrap"
+                            >
+                                {map.creator}
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Box>
-                
-                <Paper elevation={2} sx={{ height: "100px", display: "flex", flexDirection: "column", borderRadius: "0 0 6px 6px", boxShadow: 0 }}>
-                    <Divider />
-                    <Typography variant="subtitle2" color="textSecondary">
-                        {map.creator}
-                    </Typography>
-                </Paper>
             </Box>
-            
         </Grid>
     )
 }
@@ -89,7 +231,7 @@ interface MapBrowserProps {
     setPage: (page: number) => void
 }
 
-const PAGE_SIZE = 15;
+const PAGE_SIZE = 18;
 
 function MapBrowser(props: MapBrowserProps) {
     const { maps, page, setPage } = props;
@@ -112,7 +254,7 @@ function MapBrowser(props: MapBrowserProps) {
             <Grid container spacing={2} justifyContent="center">
                 {items}
             </Grid>
-            <Box display="flex" justifyContent="center">
+            <Box display="flex" justifyContent="center" mt={2}>
                 <Pagination 
                     count={count} 
                     page={page} 
@@ -157,7 +299,6 @@ function MapSearchBar(props: MapSearchBarProps) {
 
 function MapsHome() {
     const { sortedMaps } = useOutletContext() as ContextParams;
-    const theme = useTheme();
     
     const [ page, setPage ] = useQueryState("page", 
         parseAsInteger
