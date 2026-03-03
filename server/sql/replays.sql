90594b5f3671145251817b73415e2fa77b90bc70
Carter Penterman <carterpenterman@gmail.com>
2026-03-03 10:46:28 -0600

    and this should be in the right direction

--- server/sql/replays.sql
+++ server/sql/replays.sql
@@ -4,6 +4,6 @@ CREATE TABLE replay_views (
     user_id BIGINT,
     viewed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     ip_address VARBINARY(16) NOT NULL,
-    INDEX view_log_idx (time_id, viewed_at, ip_address),
+    INDEX view_log_idx (time_id, viewed_at DESC, ip_address),
     INDEX recent_user_views_idx (user_id, viewed_at DESC)
 );
